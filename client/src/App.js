ac32345252226125eeb55b9b0bdaac32a8cae92f
Rashida Kapadia <rashidamk21@gmail.com>
2024-05-17 21:21:11 -0500

    creating client and server folders

---
+++ client/src/App.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  ApolloClient,
+  ApolloProvider,
+  InMemoryCache,
+  createHttpLink
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+import "./App.css";
+
+const httpLink = createHttpLink({
+  uri: "/graphql",
+});
+
+// SETTING UP THE CONTEXT
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem("id_token");
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
+function App() {
+  return (
+    <ApolloProvider client={client}>
+      <Router>
+        <>
+          <Routes>
+            <Route exact path='/' element={<Login />} />
+            <Route path='/login' element={<Login />} />
+          </Routes>
+        </>
+      </Router>
+    </ApolloProvider>
+  );
+}
+
+export default App;
